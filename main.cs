2f91993b11444c40e083e866df1a2c02785dda82
Ambal_Ced <147784891+Ambal-Ced@users.noreply.github.com>
2024-06-16 13:52:40 +0000

    Much More about Decimal

--- main.cs
+++ main.cs
@@ -235,6 +235,99 @@ namespace main
             Console.WriteLine(7 + secondNumber);
             Console.WriteLine("??" + 7 + secondNumber);
             Console.WriteLine("??" + (7 + secondNumber));
+            Console.WriteLine("\n\n");
+
+            // initialize variables - graded assignments 
+            int currentAssignments = 5;
+
+            int sophia1 = 93;
+            int sophia2 = 87;
+            int sophia3 = 98;
+            int sophia4 = 95;
+            int sophia5 = 100;
+
+            int nicolas1 = 80;
+            int nicolas2 = 83;
+            int nicolas3 = 82;
+            int nicolas4 = 88;
+            int nicolas5 = 85;
+
+            int zahirah1 = 84;
+            int zahirah2 = 96;
+            int zahirah3 = 73;
+            int zahirah4 = 85;
+            int zahirah5 = 79;
+
+            int jeong1 = 90;
+            int jeong2 = 92;
+            int jeong3 = 98;
+            int jeong4 = 100;
+            int jeong5 = 97;
+
+            decimal sophiagrade = ((decimal)sophia1 + (decimal)sophia2 + (decimal)sophia3 + (decimal)sophia4 + (decimal)sophia5) / 5;
+            decimal nicolasgrade = ((decimal)nicolas1 + (decimal)nicolas2 + (decimal)nicolas3 + (decimal)nicolas4 + (decimal)nicolas5) / 5;
+            decimal zahiragrade = ((decimal)zahirah1 + (decimal)zahirah2 + (decimal)zahirah3 + (decimal)zahirah4 + (decimal)zahirah5) / 5;
+            decimal jeonggrade = ((decimal)jeong1 + (decimal)jeong2 + (decimal)jeong3 + (decimal)jeong4 + (decimal)jeong5) / 5;
+
+            Console.WriteLine("Student\t\tGrade");
+            Console.WriteLine("Sophia\t\t" + sophiagrade + "\tA");
+            Console.WriteLine("Nicolas\t\t" + nicolasgrade + "\tB");
+            Console.WriteLine("Zahirah\t\t" + zahiragrade + "\tB");
+            Console.WriteLine("Jeong\t\t" + jeonggrade + "\tA");
+
+            //getting the digit
+            string studentName = "Sophia Johnson";
+            string course1Name = "English 101";
+            string course2Name = "Algebra 101";
+            string course3Name = "Biology 101";
+            string course4Name = "Computer Science I";
+            string course5Name = "Psychology 101";
+
+            int course1Credit = 3;
+            int course2Credit = 3;
+            int course3Credit = 4;
+            int course4Credit = 4;
+            int course5Credit = 3;
+
+            int gradeA = 4;
+            int gradeB = 3;
+
+            int course1Grade = gradeA;
+            int course2Grade = gradeB;
+            int course3Grade = gradeB;
+            int course4Grade = gradeB;
+            int course5Grade = gradeA;
+
+            int totalCreditHours = 0;
+            totalCreditHours += course1Credit;
+            totalCreditHours += course2Credit;
+            totalCreditHours += course3Credit;
+            totalCreditHours += course4Credit;
+            totalCreditHours += course5Credit;
+
+            int totalGradePoints = 0;
+            totalGradePoints += course1Credit * course1Grade;
+            totalGradePoints += course2Credit * course2Grade;
+            totalGradePoints += course3Credit * course3Grade;
+            totalGradePoints += course4Credit * course4Grade;
+            totalGradePoints += course5Credit * course5Grade;
+
+            decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+
+            int leadingDigit = (int)gradePointAverage;
+            int firstDigit = (int)(gradePointAverage * 10) % 10;//to get the first decimal number use x 10 %10 
+            int secondDigit = (int)(gradePointAverage * 100) % 10;// to get second decimal digit use x100
+
+            Console.WriteLine($"Student: {studentName}\n");
+            Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+            Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+            Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+            Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+            Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+            Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+            Console.WriteLine($"Final GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+            Console.WriteLine($"Final GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+
         }
     }
 }
